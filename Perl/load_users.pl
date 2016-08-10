5633c09b59b0fe9b27ac1ff5bd65eb6786338111
Louis-Nicolas Gauthier <gauthier.louis.nicolas@gmail.com>
2016-08-10 14:42:18 -0700

    Loads the values in DB

--- Perl/load_users.pl
+++ Perl/load_users.pl
@@ -4,18 +4,23 @@
 #
 # Lecture du fichier passwd
 use DBI;
+use warnings;
 
-# Quand dois-je fermer le fichier?:w
-open(PASSWD, '/etc/passwd');
-while (<PASSWD>)
-{
-    ($nom_user, $id_user, $id_group, $gecos, $working_dir, $exec)=split(/:/);
-}
-
-# Chargement des données dans la table <users> de la DB <comptes_linux>.
-# Est-ce que je dois utiliser un compte local? Ou utiliser un compte créé dans Mariadb?
 $dsn = "DBI:mysql:comptes_linux";
 $username = "root";
 $password = "crosemont";
-$dbh = DBI->connect($dsn,$username,$password);
- print "Connecté à la base de données\n";
+$dbh = DBI->connect( $dsn, $username, $password
+                        { RaiseError => 1, AutoCommit => 0});
+# print "Connecté à la base de données\n";
+
+$sth = $dbh->prepare("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)"); # sth = Statement Handle Object
+
+open(PASSWD, '/etc/passwd') or die "The file could not be opened: 
+Error $?: $!";
+
+while (<PASSWD>) {
+    ($nom_user, $x, $id_user, $id_group, $gecos, $working_dir, $exec)=split(/:/);
+    $sth->execute( $nom_user, $id_user, $id_group, $gecos, $working_dir, $exec);
+}    
+
+close PASSWD;
