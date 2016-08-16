dc1b58d5bfcce190e6cf155318673b5066657f38
root <gauthier.louis.nicolas@gmail.com>
2016-08-16 16:16:11 -0700

    Ajout de info_users.pl

--- Perl/info_users.pl
+++ Perl/info_users.pl
@@ -14,18 +14,31 @@ $dbh = DBI->connect( $dsn, chomp($username), chomp($password),
                         { RaiseError => 1, AutoCommit => 0});
 
 # Exeécution des instructions mysql en utilisant le module DBI
-$sth = $dbh->prepare("SELECT * FROM users"); # sth = Statement Handle Object
-$sth->execute();
+$sth = $dbh->prepare("SELECT ? FROM users"); # sth = Statement Handle Object
+print "Entrez un des critères selon lequel afficher les résultats [nom, uid, gid ou programme]: ";
+chomp($user_input = <STDIN>);
+# La fonction uppercase (UC) permet d'ignorer la casse
+if (uc($user_input) eq "NOM") {
+		$sth->execute($nom_user)
+	} elsif (uc($user_input) eq "UID") {
+		$sth->execute($id_user)
+	} elsif (uc($user_input) eq "GID") {
+		$sth->execute($id_group)
+	}elsif (uc($user_input) eq "PROGRAMME") {
+		$sth->execute($exec)
+	} else { 
+		print "Erreur: Veuillez entrer un des choix proposés.\n" 
+};
 
-# Affichage du contenu de la table USERS
-$format = '%-20s %11s %11s %70s %30s %15s';
-print "\n\nRÉSULTATS\n\n";
-print "=" x 184 . "\n";
-
-printf ( $format, "\nnom_user", "id_user", "id_group", "gecos", "working_dir", "exec\n");
-print "\n" . "-" x 184 . "\n";
-while (@row = $sth->fetchrow_array()){
-    ($nom_user, $id_user, $id_group, $gecos, $working_dir, $exec) = @row;
-    printf ( $format, @row);
-}
-$dbh->disconnect;
+## Affichage du contenu de la table USERS
+#$format = '%-20s %11s %11s %70s %30s %15s';
+#print "\n\nRÉSULTATS\n\n";
+#print "=" x 184 . "\n";
+#
+#printf ( $format, "\nnom_user", "id_user", "id_group", "gecos", "working_dir", "exec\n");
+#print "\n" . "-" x 184 . "\n";
+#while (@row = $sth->fetchrow_array()){
+#    ($nom_user, $id_user, $id_group, $gecos, $working_dir, $exec) = @row;
+#    printf ( $format, @row);
+#}
+#$dbh->disconnect;
